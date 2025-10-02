2b03ffe7f11ec2beaaa60dd05e440e3679d78082
SoumyaRanjanBhana1 <soumyabhanja180@gmail.com>
2025-10-02 21:49:57 +0530

    server

--- models/payment.js
+++ models/payment.js
@@ -1,15 +1,28 @@
-import mongoose from 'mongoose';
+// src/models/Payment.js
+import mongoose from "mongoose";
 
 const paymentSchema = new mongoose.Schema({
-  orderId: String,
-  paymentId: String,
-  signature: String,
-  amount: Number,
-  verified: Boolean,
-  timestamp: {
-    type: Date,
-    default: Date.now,
+  orderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Order",
+    required: true,
   },
-});
+  paymentId: {
+    type: String,
+    required: true,
+  },
+  signature: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "success",
+  },
+}, { timestamps: true });
 
-export default mongoose.model('Payment', paymentSchema);
+export default mongoose.model("Payment", paymentSchema);
