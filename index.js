4ceb8d31396a782adf38b46196e517573f484428
SOUMYA RANJAN BHANJA <soumyabhanja180@gmail.com>
2025-10-31 11:43:21 +0530

    Fix CORS configuration for production server

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(cors({
   origin: "http://localhost:5173",
-  https://eshopnew-server-4.onrender.com,
+  "https://eshopnew-server-4.onrender.com",
   credentials: true,
 }));
 app.use(express.json());
